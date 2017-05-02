8073e2e95e963dc478e620dc2012c34b87fc7581
James Gallagher <jgallagher@opendap.org>
2017-05-01 20:08:15 -0600

    Added new docs

---
+++ html/classlibdap_1_1Byte.js
@@ -0,0 +1,25 @@
+var classlibdap_1_1Byte =
+[
+    [ "Byte", "classlibdap_1_1Byte.html#a6b22d844cd6418102466954f8e1e3106", null ],
+    [ "Byte", "classlibdap_1_1Byte.html#aa0470214af169a5e1c080baa8257fbba", null ],
+    [ "~Byte", "classlibdap_1_1Byte.html#a85d098199c9f39f50e205a9da566c561", null ],
+    [ "Byte", "classlibdap_1_1Byte.html#a5931c171a88042d65f65117fc237e5ac", null ],
+    [ "buf2val", "classlibdap_1_1Byte.html#a2de44757203d23419d59c8731ca0b96c", null ],
+    [ "compute_checksum", "classlibdap_1_1Byte.html#a9aceffe3fc1b898adf50ce44151c2444", null ],
+    [ "d4_ops", "classlibdap_1_1Byte.html#a44a8efbbd1e0423b4959bc76baf92dfd", null ],
+    [ "deserialize", "classlibdap_1_1Byte.html#a11a4fa1e2e5c6477c38af9fa5c3118b4", null ],
+    [ "deserialize", "classlibdap_1_1Byte.html#a9ffbfab78e18db036bfcf03fd0afc05c", null ],
+    [ "dump", "classlibdap_1_1Byte.html#a3cf81110f37a9b2d31a491de936b6d51", null ],
+    [ "operator=", "classlibdap_1_1Byte.html#a38a34e521485f8c9c436b9cf1d44c3b9", null ],
+    [ "ops", "classlibdap_1_1Byte.html#ad97b380875f85f6e7c3d02488f209afa", null ],
+    [ "print_val", "classlibdap_1_1Byte.html#a54af53582af918c4a94ad98ba9845630", null ],
+    [ "print_val", "classlibdap_1_1Byte.html#adc3565bb42fd7454205e61a8f16119e4", null ],
+    [ "ptr_duplicate", "classlibdap_1_1Byte.html#a599c7dec30fe35df45c9a66cbb7030be", null ],
+    [ "serialize", "classlibdap_1_1Byte.html#aa70aeb6aaddd570ed5bc32215b53c88e", null ],
+    [ "serialize", "classlibdap_1_1Byte.html#a2799f2bb1a9f1992ec57c7f31761abe1", null ],
+    [ "set_value", "classlibdap_1_1Byte.html#ad79ed596e502d99c450a8a1e23b381b7", null ],
+    [ "val2buf", "classlibdap_1_1Byte.html#a8b8afd3348f7d22e6ff02c0c605c7bfd", null ],
+    [ "value", "classlibdap_1_1Byte.html#a96312bb8da28d27316a0077143e42360", null ],
+    [ "width", "classlibdap_1_1Byte.html#a4d4eb99a6d1022ca4e1808776438399d", null ],
+    [ "d_buf", "classlibdap_1_1Byte.html#a47a666acf4bc1d3a2f4beb7f2199cb90", null ]
+];
